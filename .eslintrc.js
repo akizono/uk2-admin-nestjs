e875ccece9940cdd48cc1772ef8d4ba7a84ef311
akizono <aiyedoki@gmail.com>
2024-12-24 16:09:49 +0800

    feat: 建立 NestJS 專案

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier' // 關閉 ESLint 中與 Prettier 可能衝突的規則
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', '*.json', '*.yaml', '*.yml', '*.md', 'LICENSE'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+        // trailingComma: 'none',
+        printWidth: 120,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        quoteProps: 'preserve',
+        endOfLine: 'lf',
+      },
+    ],
+  },
+};
+
