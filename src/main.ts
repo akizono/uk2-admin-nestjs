13d3203f9eade10daad792047cd374be04144d9e
akizono <aiyedoki@gmail.com>
2025-01-04 23:51:56 +0800

    feat: 新增跨來源資源共用設定
    - 在 .env 加入 CORS 相關環境變數
    - 在 main.ts 加入 CORS 設定

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
   // 全局異常過濾器
   app.useGlobalFilters(new HttpExceptionFilter())
 
+  // 跨域
+  app.enableCors({
+    origin: EnvHelper.getString('CORS_ORIGIN'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: EnvHelper.getBoolean('CORS_CREDENTIALS'),
+  })
+
   await app.listen(EnvHelper.getNumber('SERVER_PORT'))
 }
 bootstrap()
