9eec79df08a707dfd3aff1fa0761bccf7989aa18
akizono <aiyedoki@gmail.com>
2025-01-20 16:08:38 +0800

    feat: 新增使用者關聯角色時的衝突檢查

--- src/modules/system/user-role/user-role.service.ts
+++ src/modules/system/user-role/user-role.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common'
+import { ConflictException, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 
@@ -12,7 +12,12 @@ export class UserRoleService {
     private readonly userRoleRepository: Repository<UserRoleEntity>,
   ) {}
 
-  create(createUserRoleDto: CreateUserRoleDto) {
+  async create(createUserRoleDto: CreateUserRoleDto) {
+    const { userId, roleId } = createUserRoleDto
+
+    const userRole = await this.userRoleRepository.findOne({ where: { userId, roleId } })
+    if (userRole) throw new ConflictException('使用者已經綁定了該角色')
+
     return this.userRoleRepository.save(createUserRoleDto)
   }
 }
