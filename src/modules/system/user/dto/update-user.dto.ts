3b72b601168b49619c372862b5b907f25b6a6809
akizono <aiyedoki@gmail.com>
2025-01-21 17:09:02 +0800

    feat: 更新 UpdateUserDto 以排除密碼欄位

--- src/modules/system/user/dto/update-user.dto.ts
+++ src/modules/system/user/dto/update-user.dto.ts
@@ -1,9 +1,12 @@
-import { PartialType } from '@nestjs/mapped-types'
+import { PartialType, OmitType } from '@nestjs/mapped-types'
 import { IsNotEmpty } from 'class-validator'
 
 import { CreateUserDto } from './create-user.dto'
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+/** 排除 password 欄位 */
+const UserDtoWithoutPassword = OmitType(CreateUserDto, ['password'] as const)
+
+export class UpdateUserDto extends PartialType(UserDtoWithoutPassword) {
   @IsNotEmpty({ message: 'id不能為空' })
   id: string
 }
