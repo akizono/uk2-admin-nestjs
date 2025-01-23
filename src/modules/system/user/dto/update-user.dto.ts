fdfac749d13551682038c772b282ee429325c11d
akizono <aiyedoki@gmail.com>
2025-01-23 10:30:38 +0800

    feat: 更新 UpdateUserDto 以禁止使用者更新 username

--- src/modules/system/user/dto/update-user.dto.ts
+++ src/modules/system/user/dto/update-user.dto.ts
@@ -6,10 +6,10 @@ import { ParseBigIntPipe } from 'src/common/pipes/parse-bigInt-pipe'
 import { CreateUserDto } from './create-user.dto'
 import { ApiProperty } from '@nestjs/swagger'
 
-/** 排除 password 欄位 */
-const UserDtoWithoutPassword = OmitType(CreateUserDto, ['password'] as const)
+/** 排除 username 和 password 欄位 */
+const UserDtoWithoutCredentials = OmitType(CreateUserDto, ['username', 'password'] as const)
 
-export class UpdateUserDto extends PartialType(UserDtoWithoutPassword) {
+export class UpdateUserDto extends PartialType(UserDtoWithoutCredentials) {
   @ApiProperty({ description: '需要更新的使用者 ID' })
   @IsNotEmpty({ message: 'id不能為空' })
   @Transform(({ value }) => new ParseBigIntPipe().transform(value))
